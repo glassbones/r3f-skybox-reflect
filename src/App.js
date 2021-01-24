657ef2836e90ac338631cc4520990a1fa43594a9
Michael <glassbonesofficial@gmail.com>
2021-01-24 12:49:07 -0600

    setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import "./styles.css";
+import React from 'react'
+import { Canvas } from "react-three-fiber"
+
+//const SkyBox =()=> 
+
+const Sphere =()=> 
+<mesh 
+  visible position={[0,0,0]} 
+  rotation={[0,0,0]}
+  castShadow>
+
+  <sphereGeometry 
+    attach='geometry' 
+    args={[2,32,32]}/>
+  <meshBasicMaterial
+    attach='material'
+    color='red'
+    roughness={0.1}
+    metalness={1}/>
+
+</mesh>
+
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Canvas>
+      <Sphere/>
+    </Canvas>
   );
 }
